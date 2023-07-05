1fd1d5813527ba938651451b9fc89900f2345799
ThomasBacha <73293720+ThomasBacha@users.noreply.github.com>
2023-07-04 22:09:05 -0500

    adding starter pack commit

---
+++ src/components/TestComp.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+export const TestComp = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-6xl font-bold">
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+            <h1>Test Component</h1>
+            You may use this to follow along <br>
+            </br>
+            <a  href = "https://www.youtube.com/watch?v=pUNSHPyVryU">Click here for the tutorial </a>
+
+
+        </div>
+    );
+    }
+
+export default TestComp;
